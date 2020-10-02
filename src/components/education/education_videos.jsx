c7e549f55cd9da0957632d4b406b3dd7bb10d300
johnstewart0820 <john.s@outsource.codes>
2020-10-02 10:13:32 +0300

    added: film education

---
+++ src/components/education/education_videos.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+
+const EducationVideos = (props) => {
+  const videos = props.data;
+
+  return (
+    <>
+      <article className="section single-item-page mb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {videos && videos.field_educational_videos_title  && 
+                <h3>
+                  {videos.field_educational_videos_title.split(" ")[0]}
+                  <br/>
+                  {videos.field_educational_videos_title.split(" ")[1]
+                }</h3>
+              }
+            </div>
+            <div className="row col-12 mt-4">
+              <div className="col-6">
+                {videos && videos.field_educational_videos && 
+                  <video poster={videos.field_educational_videos[0].field_educational_videos_image} alt="" className="img-full" 
+                    src={videos.field_educational_videos[0].field_educational_videos_link} controls
+                  />
+                }
+              </div>
+              <div className="col-6">
+                <div className="row education-top-image">
+                  <div className="col-6">
+                    {videos && videos.field_educational_videos && 
+                      <video poster={videos.field_educational_videos[1].field_educational_videos_image} alt="" className="img-full" 
+                        src={videos.field_educational_videos[1].field_educational_videos_link} controls
+                      />  
+                    }
+                  </div>
+                  <div className="col-6">
+                    {videos && videos.field_educational_videos && 
+                      videos.field_educational_videos[1].field_educational_videos_description
+                    }
+                  </div>
+                </div>
+                <div className="row education-bottom-image">
+                  <div className="col-6">
+                    {videos && videos.field_educational_videos && 
+                      <video poster={videos.field_educational_videos[2].field_educational_videos_image} alt="" className="img-full" 
+                        src={videos.field_educational_videos[2].field_educational_videos_link} controls
+                      />  
+                    }
+                  </div>
+                  <div className="col-6">
+                    {videos && videos.field_educational_videos && 
+                      videos.field_educational_videos[2].field_educational_videos_description
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  )
+};
+
+export default withRouter(EducationVideos);
--- src/pages/Education/Education_Industriada.jsx
+++ src/pages/Education/Education_Industriada.jsx
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import Axios from "axios";
+import Loader from "../../components/general/Loader";
+
+import EducationVideos from "../../components/education/education_videos";
+import EducationSocialMedia from "../../components/education/education_social_media";
 
 const EducationIndustriada = (props) => {
-    const {history} = props;
-
+    const [info, setInfo] = useState({});
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        Axios.get("http://api.ustron.s3.netcore.pl/contents/posts/238")
+        .then((response) => {
+            setInfo(response.data.content);
+            setLoading(false);
+        });
+    }, [])
+    
     return (
         <>
-        EducationIndustriada
+            {loading && <Loader/>} 
+            {!loading && 
+            <>
+                <EducationVideos data={info && info.acf}></EducationVideos>
+                <EducationSocialMedia data={info && info.acf}></EducationSocialMedia>
+            </>
+            }
         </>
     );
 }
