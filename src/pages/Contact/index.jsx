4fa619ea2bf675177d4e31d735b9b3605ed5d423
johnstewart0820 <john.s@outsource.codes>
2020-10-02 04:04:13 +0300

    added: contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import { ToastProvider } from 'react-toast-notifications';
+import Axios from "axios";
+import Loader from "../../components/general/Loader";
+
+
+import ContactForm from "../../components/contact/contact-form";
+import Description from "../../components/contact/description";
+import LocationMap from "../../components/contact/map";
+import ObjectInfo from "../../components/contact/object-info";
 
 const Contact = (props) => {
     const {history} = props;
+    const [info, setInfo] = useState({});
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
+        Axios.get("http://api.ustron.s3.netcore.pl/contents/posts/241")
+        .then((response) => {
+            setInfo(response.data.content);
+            setLoading(false);
+        });
     }, [])
     return (
-        <>
-        {window.location.href}
-        </>
+        <ToastProvider>
+           {loading && <Loader/>} 
+           {!loading && 
+            <>
+                <Description data={info} />
+                <ObjectInfo data={info} />
+                <ContactForm />
+                <LocationMap data={info}/>
+            </>
+           }
+        </ToastProvider>
     );
 }
 
