86d85afafe24f6ee22154097dd129b73cb6a2aef
johnstewart0820 <john.s@outsource.codes>
2020-10-15 20:36:04 +0300

    modify: fix background issue on 404/500

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,14 +5,14 @@ import Breadcrumbs from "../components/general/Breadcrumbs";
 import {Link} from "react-router-dom";
 import Utils from "../utils/Locale";
 import { main_url } from "../extra/main_menu";
-
+import DefaultImage from '../img/forest.jpg';
 export default function NotFoundPage() {
     const locale = Utils.getLocale();
     return (
         <MainHeaderSection>
             <Breadcrumbs breadcrumbs={[{label: "Start", href: main_url},{label: "Muzeum Ustroński", to: "/"}, {label: "404"}]} needOperate = {false}/>
             <PageHeaderSection extra_classes="not-found-page" noDefaultImage>
-                <div className="background-error">
+                <div className="background-error" style={{backgroundImage:`url(${DefaultImage})`}}>
                     <div className="page-title">
                         404<br/>
                         <p>
