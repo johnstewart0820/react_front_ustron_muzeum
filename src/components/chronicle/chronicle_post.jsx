4e33bf7464a0f3dead4aefbae097a35d770d3e2d
johnstewart0820 <john.s@outsource.codes>
2020-10-15 13:08:55 -0700

    complete responsive without education

--- src/components/chronicle/chronicle_post.jsx
+++ src/components/chronicle/chronicle_post.jsx
@@ -12,11 +12,11 @@ const ChroniclePost = (props) => {
                 <div className="col-12">
                 {chronicle && chronicle.title  && <h3>{chronicle.title}</h3>}
                 </div>
-                <div className="row col-12">
-                <div className="col-4">
+                <div className="row">
+                <div className="col-12 col-md-4">
                     {chronicle && chronicle.title && <img src={chronicle.image} alt="" className="img-fluid" />}
                 </div>
-                <div className="col-8 chronicle-post">
+                <div className="col-12 col-md-8 chronicle-post">
                     <div dangerouslySetInnerHTML={{__html: chronicle && chronicle.body}}></div>
                 </div>
                 </div>
